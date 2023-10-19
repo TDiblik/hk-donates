2f3e6179b1a3d19f28fbeb60186ea31cebee718f
Michal Vaniš <zirixcz@gmail.com>
2023-10-19 17:15:51 +0200

    feat(frontend): category selection on donation page

--- client/src/modules/Donate/components/CategorySelect/CategorySelect.tsx
+++ client/src/modules/Donate/components/CategorySelect/CategorySelect.tsx
@@ -1,6 +1,7 @@
 import { Checkbox } from "@chakra-ui/react";
 import { category } from "../../types";
 import styles from "./CategorySelect.module.css";
+import { subCategoriesMock } from "./constants";
 
 interface Props {
   categories: category[];
@@ -17,13 +18,44 @@ const CategorySelect = (props: Props) => {
           {props.categories.map((category: category) => {
             return (
               <div className={styles.categoryItem}>
-                <Checkbox borderColor={"#C4001F"} defaultChecked></Checkbox>
-                <p>{category.name}</p>
+                <div className={styles.categoryItemWrapper}>
+                  <Checkbox colorScheme="red" defaultChecked></Checkbox>
+                  <p>{category.name}</p>
+                </div>
+                <img src="./assets/FiChevronRight.svg" role="none" alt="" />
               </div>
             );
           })}
         </div>
-        <div className={styles.categoryPreview}></div>
+        <div className={styles.categoryPreview}>
+          <p className={styles.blueParagraph}>{">‘category.name’"}</p>
+          <table border={1}>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {subCategoriesMock.map((subCategory, index) => {
+                return (
+                  <tr key={index}>
+                    <td>
+                      <Checkbox colorScheme="red" defaultChecked></Checkbox>
+                    </td>
+                    <td>{subCategory.heading}</td>
+                    <td>{subCategory.description}</td>
+                    <td>
+                      <button>Action</button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
